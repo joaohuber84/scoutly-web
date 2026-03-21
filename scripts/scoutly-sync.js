88bd13ce5308f36abe50a10fb82c9b0f253def76
joaohuber84 <joaohuber84@gmail.com>
2026-03-20 21:44:35 -0400

    Update scoutly-sync.js

--- scripts/scoutly-sync.js
+++ scripts/scoutly-sync.js
@@ -14,7 +14,7 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 const API = "https://v3.football.api-sports.io"
 const TIMEZONE = "America/Sao_Paulo"
 const WINDOW_HOURS = 72
-const WINDOW_MODE = "UNTIL_SUNDAY"
+const WINDOW_MODE = "NEXT_HOURS"
 const REQUEST_DELAY_MS = 1200
 
 const TARGET_COMPETITIONS = [
@@ -246,15 +246,7 @@ function getSyncWindowRange() {
   const start = new Date(now)
   start.setHours(0, 0, 0, 0)
 
-  if (WINDOW_MODE === "UNTIL_SUNDAY") {
-    const end = new Date(now)
-    const day = end.getDay()
-    const daysUntilSunday = (7 - day) % 7
-    end.setDate(end.getDate() + daysUntilSunday)
-    end.setHours(23, 59, 59, 999)
-    return { start, end }
-  }
-
+if (WINDOW_MODE === "NEXT_HOURS") {
   const end = new Date(now.getTime() + WINDOW_HOURS * 60 * 60 * 1000)
   return { start, end }
 }
